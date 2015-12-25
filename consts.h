b7c19e37ee3d63dfda5a22cacfdc6314b0e0345f
Ian McLinden <mcli0027@umn.edu>
2015-12-24 19:59:28 -0600

    Extern'd ints

--- consts.h
+++ consts.h
@@ -69,6 +69,24 @@
 
 /* ---- Globals ------------------------------------------------------------------------------------------------------*/
 
+#ifdef  CDROM_PLOTTER_PLOTTER_H
+// Terminal GUI Variables
+int MaxRows = 24;
+int MaxCols = 80;
+int MessageX = 1;
+int MessageY = 24;
+unsigned char MoveBuffer[BUFFERSIZE];
+
+// Stepper Motor Drivers
+int StepX = 0;
+int StepY = 0;
+double StepsPermmX = 250.0 / 35.0;
+double StepsPermmY = 250.0 / 35.0;
+
+// Input Filepath
+char PicturePath[1000];
+
+#else
 // Terminal GUI Variables
 extern int MaxRows = 24;
 extern int MaxCols = 80;
